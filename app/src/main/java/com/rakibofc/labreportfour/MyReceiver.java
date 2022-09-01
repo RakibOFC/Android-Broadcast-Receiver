e5b2899ca6367e4357b0a814af5b19be2396890d
RakibOFC <rakibull2000@gmail.com>
2022-09-01 19:44:41 +0600

    Add files and folder in GIT

--- app/src/main/java/com/rakibofc/labreportfour/MyReceiver.java
+++ app/src/main/java/com/rakibofc/labreportfour/MyReceiver.java
@@ -3,7 +3,6 @@ package com.rakibofc.labreportfour;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 import android.widget.Toast;
 
 public class MyReceiver extends BroadcastReceiver {
@@ -12,6 +11,7 @@ public class MyReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
 
         if(intent.getAction().equals("android.intent.action.BATTERY_CHANGED")){
+
             Toast.makeText(context, "Battery Status Changes", Toast.LENGTH_SHORT).show();
         }
     }
